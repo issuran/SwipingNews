53ff4cc5bb90f8aa7a76bad58a73162b8a440e20
Tiago Oliveira <s2it_tioliveira@uolinc.com>
2018-12-19 15:11:44 -0200

    Swiping news collection view cell

---
+++ Swiping News/News/View/Cell/SwipingNewsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SwipingNewsCollectionViewCell.swift
+//  Swiping News
+//
+//  Created by Tiago Oliveira on 19/12/18.
+//  Copyright © 2018 Optimize 7. All rights reserved.
+//
+
+import UIKit
+
+class SwipingNewsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
