4c45c67f8113035f8ab5043c1759af770818d1b5
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-09-17 20:35:16 -0300

    Created splash view controller to use lottie

---
+++ Swiping News/Sections/Splash/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  Swiping News
+//
+//  Created by Tiago Oliveira on 17/09/19.
+//  Copyright © 2019 Optimize 7. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
