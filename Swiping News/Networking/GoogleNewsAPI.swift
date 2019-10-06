8ac06cca865c7105170e23f4c480f2ef814c034c
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-10-06 18:57:53 -0300

    Attempt to add keys without display on github

--- Swiping News/Networking/GoogleNewsAPI.swift
+++ Swiping News/Networking/GoogleNewsAPI.swift
@@ -13,6 +13,17 @@ enum GoogleNewsAPI {
 }
 
 extension GoogleNewsAPI: ServiceProtocol {
+    var api_key: String {
+        if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
+            let keys = NSDictionary(contentsOfFile: path)
+            if let dict = keys {
+                let api_key = dict["API_KEY"] as! String
+                return api_key
+            }
+        }
+        return ""
+    }
+    
     var scheme: String {
         return "https"
     }
@@ -39,7 +50,7 @@ extension GoogleNewsAPI: ServiceProtocol {
         switch self {
         case .topHeadlines(let country):
             let parameters = ["country": country,
-                              "apiKey": Keys.API_KEY]
+                              "apiKey": api_key]
             return .requestUrlParameters(urlParameters: parameters)
         }
     }
